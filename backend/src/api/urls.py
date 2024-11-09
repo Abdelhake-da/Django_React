6a75cda1a1ac7e38e20d5f2836bd4fed622f106c
Abdelhake-da <abdelhake.da@gmail.com>
2024-11-09 15:05:02 +0100

    - install axios - create js file for axios call with generic parameters - modify our create.js file - test the outcomes - navigate to a different page after submission

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -3,6 +3,6 @@ from .views import home, ProjectViewSet
 from rest_framework.routers import DefaultRouter
 app_name = 'api'
 router = DefaultRouter()
-router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'project', ProjectViewSet, basename='project')
 
 urlpatterns = router.urls
--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -5,11 +5,34 @@ import MySelectField from './forms/MySelectField'
 import MyDatePickerField from './forms/MyDatePickerField'
 import MyMultilineField from './forms/MyMultilineField'
 import { useForm } from 'react-hook-form'
+import AxiosInstance from './Axios'
+import Dayjs from 'dayjs'
+import { useNavigate } from 'react-router-dom'
 const Create = () => {
+    const navigate = useNavigate()
     const width = '30%'
-    const { handleSubmit, reset, setValue, control } = useForm();
+    const defaultValue = {
+        name: '',
+        comments: '',
+        status: '',
+        start_date: '',
+        end_date: '',
+    }
+    const { handleSubmit, reset, setValue, control } = useForm({ defaultValues: defaultValue });
     const submission = (data) => {
-        console.log(data)
+        const StartDate = Dayjs(data.start_date["$d"]).format('YYYY-MM-DD')
+        const EndDate = Dayjs(data.end_date["$d"]).format('YYYY-MM-DD')
+        AxiosInstance.post('project/',
+            {
+                name: data.name,
+                comments: data.comments,
+                status: data.status,
+                start_date: StartDate,
+                end_date: EndDate,
+            }
+        ).then((res) => {
+            navigate('/')
+        })
     }
     return (
         <div>
